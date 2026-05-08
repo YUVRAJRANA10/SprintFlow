8cff2322a6b9b24e871b47b9e9a1a3bf62f1d04b
Yuvraj <yuvraj.r0810@gmail.com>
2026-05-08 23:30:29 +0530

    feat: add auth routes and JWT handling

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 import connectDB from "./config/db.js"
+
+import authRoutes from "./routes/authRoutes.js"
 dotenv.config()
 
 
@@ -11,6 +13,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
 
 app.get("/", (req,res) => {
 
